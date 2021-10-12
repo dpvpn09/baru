3d3745333e8ab2de4c786b4fbf41ce7f528436a7
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-12 13:19:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,21 +18,21 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/ssh.sh && chmod +x ssh.sh && screen -S ssh ./ssh.sh
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/ssh.sh && chmod +x ssh.sh && screen -S ssh ./ssh.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-wget https://raw.githubusercontent.com/Alamyazid/rev/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-wget https://raw.githubusercontent.com/dpvpn09/main/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/dpvpn09/rev/main/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
 rm -f /root/ssh.sh
 rm -f /root/weleh.sh
 rm -f /root/sstp.sh
@@ -59,7 +59,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/Alamyazid/rev/main/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/dpvpn09/rev/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
